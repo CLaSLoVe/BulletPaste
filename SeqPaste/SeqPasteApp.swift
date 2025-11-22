aa460e53da5fa538e5e97437d2edbb9efe24a8cb
dian <jindian_0805@foxmail.com>
2025-11-22 20:10:00 +0800

    Initial Commit

---
+++ SeqPaste/SeqPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeqPasteApp.swift
+//  SeqPaste
+//
+//  Created by Dian Jin on 22/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SeqPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
